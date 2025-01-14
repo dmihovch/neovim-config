f1d7f635438875c9939f2cd1ef0b4cd43b4abe41
Dan Mihovch <dmih@pop-os.mynetworksettings.com>
2025-01-14 15:58:39 -0500

    added some stuff

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 require("config.remaps")
 require("config.lazy")
+vim.opt.clipboard = "unnamedplus"
+vim.opt.relativenumber = true
--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -4,3 +4,8 @@ local opt = { noremap = true, silent = true }
 vim.g.mapleader = " "
 
 map("n", "<leader>ff", ":Telescope find_files<CR>", opt)
+map("v", "<C-c>", '"+y', opt)
+map("n", "<C-v>", '"+p', opt)
+
+
+
