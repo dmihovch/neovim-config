0428c7cc0d5f261b8a478d04944da2830ee4a6a8
Dan Mihovch <dmih@pop-os.mynetworksettings.com>
2025-01-15 15:43:09 -0500

    lsp works no config yet

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -4,7 +4,7 @@ local opt = { noremap = true, silent = true }
 vim.g.mapleader = " "
 
 map("n", "<leader>ff", ":Telescope find_files<CR>", opt)
-map("v", "<C-c>", '"+y', opt)
+map("v", "<C-c>", '"+yy', opt)
 map("n", "<C-v>", '"+p', opt)
 
 
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,7 @@
 return {
-	"neovim/nvim-lspconfig",
+	"VonHeikemen/lsp-zero.nvim",
 	dependencies = {
+		"neovim/nvim-lspconfig",
 		"hrsh7th/nvim-cmp",
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
@@ -10,16 +11,26 @@ return {
 		"L3MON4D3/LuaSnip",
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
+		"rafamadriz/friendly-snippets",
 	},
 	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {"ast_grep", "svelte"},
-			automatic_installation = true
-		})
-	local lspconfig = require("lspconfig")
+		local lsp = require("lsp-zero")
+		lsp.extend_lspconfig()
 
-	lspconfig.ast_grep.setup({})
-	lspconfig.svelte.setup({})
+		require("mason").setup()
+
+		require("mason-lspconfig").setup({
+			ensure_installed = { "lua_ls", "pyright", "ts_ls", "jdtls", "clangd", "gopls" },
+		})
+		require("lspconfig").lua_ls.setup{}
+		require("lspconfig").pyright.setup{}
+		require("lspconfig").ts_ls.setup{}
+		require("lspconfig").jdtls.setup{}
+		require("lspconfig").clangd.setup{}
+	--	require("lspconfig").svelte_language_server.setup{}
+		require("lspconfig").gopls.setup{}
+--		require("lspconfig").asm_lsp.setup{}
+	--	require("lspconfig").css_lsp.setup{}
+	--	require("lspconfig").html_lsp.setup{}
 	end,
 }
